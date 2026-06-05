cee12821d238c7916a853562d18e3cb8a135e6e6
Ryan Geary <7076013+theryangeary@users.noreply.github.com>
2026-06-05 00:00:00 +0000

    add gd, gD, gr, K vim maps

--- vim/.config/nvim/lua/theryangeary/init.lua
+++ vim/.config/nvim/lua/theryangeary/init.lua
@@ -5,3 +5,13 @@ vim.opt.undodir = vim.fn.stdpath("cache") .. "/undo"
 vim.opt.undofile = true
 
 vim.lsp.inlay_hint.enable(true)
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(args)
+    local buf = args.buf
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = buf })
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { buffer = buf })
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, { buffer = buf })
+    vim.keymap.set('n', 'K',  vim.lsp.buf.hover,       { buffer = buf })
+  end,
+})
