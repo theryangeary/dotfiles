f162f02563e49b7273f753e1a92d08bf3153d92f
Ryan Geary <7076013+theryangeary@users.noreply.github.com>
2026-06-17 00:00:00 +0000

    use cwd in kitty nvim hints

--- kitty/.config/kitty/nvim_hints.py
+++ kitty/.config/kitty/nvim_hints.py
@@ -52,6 +52,6 @@ def handle_result(args, data, target_window_id, boss, extra_cli_args, *a):
         nvim_cmd.append(path)
 
         shell_cmd = ' '.join(shlex.quote(a) for a in nvim_cmd)
-        boss.call_remote_control(None, ('launch', '--type=window', '--', '/bin/zsh', '-l', '-c', shell_cmd))
+        boss.call_remote_control(None, ('launch', '--type=window', '--cwd=current', '--', '/bin/zsh', '-l', '-c', shell_cmd))
     except Exception:
         _log(traceback.format_exc())
